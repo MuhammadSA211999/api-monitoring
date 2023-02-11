5c05b98b46599f1ec16532bdb7d36bba34af416e
Muhammad SA <MuhammadSA211999>
2023-02-11 21:57:15 +0600

    add update user succ

--- handler/routesHandlers/userHandler.js
+++ handler/routesHandlers/userHandler.js
@@ -84,17 +84,93 @@ handler._users.get = (requestProperties, callback) => {
     }
 }
 handler._users.put = (requestProperties, callback) => {
+    const firstName = typeof requestProperties.body.firstName === 'string' && requestProperties.body.firstName.trim().length > 0 ? requestProperties.body.firstName : false
+    const lastName = typeof requestProperties.body.lastName === 'string' && requestProperties.body.lastName.trim().length > 0 ? requestProperties.body.lastName : false
+    const phone = typeof requestProperties.body.phone === 'string' && requestProperties.body.phone.trim().length === 11 ? requestProperties.body.phone : false
+    const password = typeof requestProperties.body.password === 'string' && requestProperties.body.password.length > 0 ? requestProperties.body.password : false
 
+    if (phone) {
+        if (firstName || lastName || password) {
+            //lookup the user file by data.read method
+            data.read('users', phone, (err, uData) => {
+                const userData = { ...parseJSON(uData) }
+                if (!err && userData) {
+                    if (firstName) {
+                        userData.firstName = firstName
+                    }
+                    if (lastName) {
+                        userData.lastName = lastName
+                    }
+                    if (password) {
+                        userData.password = hash(password)
+                    }
+                    //  updating the user to db by data.update method
+                    data.update('user', phone, userData, (err) => {
+                        if (!err) {
+                            callback(200, {
+                                message: 'successfully update the user'
+                            })
+                        }
+                        else {
+                            callback(400, {
+                                error: 'Something went wrong, can not update'
+                            })
+                        }
+                    })
+                }
+                else {
+                    callback(404, {
+                        error: 'User was not found'
+                    })
+                }
+            })
+        }
+        else {
+            callback(400, {
+                error: 'Must need a update field'
+            })
+        }
+    }
+    else {
+        callback(400, {
+            error: 'Your phone is not valid'
+        })
+    }
 }
-handler._users.patch = (requestProperties, callback) => {
+// handler._users.patch = (requestProperties, callback) => {
 
-}
+// }
 
 handler._users.delete = (requestProperties, callback) => {
+    const phone = typeof requestProperties.queryStringObject.phone === 'string' && requestProperties.queryStringObject.phone.trim().length === 11 ? requestProperties.queryStringObject.phone : false
+    if (phone) {
+        //    lookup the user  by data.read
+        data.read('users', phone, (err, uData) => {
+            const userData = { ...parseJSON(uData) }
+            if (!err && userData) {
+                //delete the user from db by data.delete method 
+                data.delete('users', phone, (err) => {
+                    if (!err) {
 
-}
-handler._users.patch = (requestProperties, callback) => {
+                    }
+                    else {
 
+                    }
+                })
+            }
+            else {
+                callback(400, {
+                    error: 'there was a sever side error'
+                })
+            }
+        })
+    }
+    else {
+        callback(500, {
+            error: 'Please give a unique i phone'
+        })
+    }
 }
+
 
 module.exports = handler
